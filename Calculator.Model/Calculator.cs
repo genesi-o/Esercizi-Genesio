600f8459d5749ac90482ec7d6665900569920fdd
genesi-o <matteo.genesio@edu.itspiemonte.it>
2023-04-12 16:25:27 +0200

    First Commit

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -2,9 +2,19 @@
 {
     public class Calculator
     {
-        public object Add(int v1, int v2)
+        public int Add(int v1, int v2)
         {
             return v1+v2;
+        }
+
+        public float Add(float v1, float v2)
+        {
+            return v1+v2;
+        }
+
+        public int Sub(int v1, int v2) 
+        { 
+            return v1+v2; 
         }
     }
 }
--- Calculator.Tests/CalculatorTest.cs
+++ Calculator.Tests/CalculatorTest.cs
@@ -1,36 +1,54 @@
 ﻿using Xunit;
 using FluentAssertions;
+using System.Security.Cryptography;
 
 namespace Calculator.Tests
 {
     public class CalculatorTest
     {
-        [Fact]
-        public void AddTwoNumberSuccess()
+        [Theory]
+        [InlineData(1,2,3)]
+        public void AddTwoIntNumberSuccess(int a1, int a2, int result)
         {
             //arrange
             var calculator = new Model.Calculator();
 
             //act
-            var actualResult =calculator.Add(1,2);
+            var actualResult =calculator.Add(a1,a2);
 
             //assert
-            actualResult.Should().Be(3);
+            actualResult.Should().Be(result);
 
         }
 
-        [Fact]
-        public void AddTwoNumberSuccessAgain()
+        [Theory]
+        [InlineData(1.3,2.3,3.6)]
+        [InlineData(2.5,2.6,6.1)]
+        public void AddTwoFloatNumberSuccess(float a1, float a2, float result)
         {
             //arrange
             var calculator = new Model.Calculator();
 
             //act
-            var actualResult = calculator.Add(2, 2);
+            var actualResult = calculator.Add(a1, a2);
 
             //assert
-            actualResult.Should().Be(4);
+            actualResult.Should().Be(result);
 
+        }
+
+        [Theory]
+        [InlineData(3,2,1)]
+        public void SubTwoIntNumberSuccess(int a1, int a2, int result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Sub(a1, a2);
+
+            //assert
+            actualResult.Should().Be(result);
         }
     }
 }
